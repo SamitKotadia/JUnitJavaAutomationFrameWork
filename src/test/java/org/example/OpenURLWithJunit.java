6e6ed27f723ce78217a74b1647d9945d74bd89b8
samitkotadia <samit0304@gmail.com>
2022-04-30 13:03:39 +0100

    first commit

---
+++ src/test/java/org/example/OpenURLWithJunit.java
@@ -0,0 +1,37 @@
+package org.example;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class OpenURLWithJunit {
+    WebDriver driver;
+    @Before
+    public void setUp(){
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+    }
+    @Test
+    public void openArgos(){
+        driver.get("https://www.argos.co.uk/");
+    }
+    @Test
+    public void openNext() {
+        driver.get("https://www.next.co.uk/");
+    }
+    @Test
+    public void openPoundLand() {
+        driver.get("https://www.poundland.co.uk/");
+    }
+    @After
+    public void tearDown() throws InterruptedException {
+        Thread.sleep(5000);
+        driver.close();
+
+    }
+
+}
---
+++ src/test/java/org/example/OpenURLWithMainMethod.java
@@ -0,0 +1,18 @@
+package org.example;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class OpenURLWithMainMethod {
+    public static void main(String[] args) throws InterruptedException {
+        WebDriver driver;
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.get("https://www.argos.co.uk/");
+        driver.manage().window().maximize();
+        Thread.sleep(5000);
+
+        driver.close();
+    }
+}
